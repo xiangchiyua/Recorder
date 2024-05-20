f5c60cd53cd25aaf8ea819cc803cc2182a29ded8
cedar445 <lsk156278@outlook.com>
2024-05-21 00:42:29 +0800

    addbills初步实现

--- app/src/main/java/com/recorder/MainActivity.java
+++ app/src/main/java/com/recorder/MainActivity.java
@@ -1,21 +1,28 @@
 package com.recorder;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.recorder.databinding.ActivityMainBinding;
+import com.recorder.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +39,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
 
+    }
 }
--- app/src/main/java/com/recorder/ui/home/HomeFragment.java
+++ app/src/main/java/com/recorder/ui/home/HomeFragment.java
@@ -4,28 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.recorder.databinding.FragmentHomeBinding;
+import com.recorder.R;
+
+import java.util.zip.Inflater;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-
+    private void addBills(){
+        LinearLayout linearLayout=getView().findViewById(R.id.linearbills);
+        for(int i=1;i<=5;i++){
+            String billId="biils"+Integer.toString(i);
+            //Log.d("My", billId);
+            //Log.d("My", Integer.toString(getResources().getIdentifier(billId,"id", getActivity().getPackageName())));
+            View bill=linearLayout.findViewById(
+                    getResources().getIdentifier(billId,"id", getActivity().getPackageName()));
+            TextView text=bill.findViewById(R.id.billName);
+            text.setText(Integer.toString(i));
+        }
+    }
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        //Log.d("My","caonima");
         return root;
     }
-
+    public void onResume() {
+        super.onResume();
+        addBills();
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();

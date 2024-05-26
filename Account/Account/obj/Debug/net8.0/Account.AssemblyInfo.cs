d5eebff21faf0788e11ef8074816e765cd110ad3
xiangchiyua <161929080+xiangchiyua@users.noreply.github.com>
2024-05-26 15:50:11 +0800

    后端API部分实现
    补充了说明文档

--- Account/Account/obj/Debug/net8.0/Account.AssemblyInfo.cs
+++ Account/Account/obj/Debug/net8.0/Account.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Account")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49f40a53b83a6086b077f7a064bba3e21c86a79b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Account")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Account")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Account/readme.txt
@@ -0,0 +1,5 @@
+写了账单类、类别类、商家类、统计类（后两个还未使用）。
+对数据库的连接等进行分装，即SqlHelper类。
+对数据处理进行分装，即BillDal和CategoryDal类，在AccountController中使用。
+（MerchantDal和StatisticDal待完成）。
+实现了增删查，改还未实现。

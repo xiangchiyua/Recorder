49f40a53b83a6086b077f7a064bba3e21c86a79b
xiangchiyua <161929080+xiangchiyua@users.noreply.github.com>
2024-05-26 15:32:41 +0800

    后端API
    还未尝试

---
+++ Account/Account/Dal/CategoryDal.cs
@@ -0,0 +1,26 @@
+﻿using Account.Core;
+using Account.Model;
+using Microsoft.Data.SqlClient;
+
+namespace Account.Dal
+{
+    public class CategoryDal
+    {
+        public long insertCategory(Category category)
+        {
+            string sql = "INSERT INTO category(name,description) VALUES('@name','@description')";
+            return SqlHelper.ExecuteNonQuery(sql,
+                new SqlParameter[]
+                {
+                    new SqlParameter("@name",category.Name),new SqlParameter("@description",category.Description),
+                });
+        }
+
+        public long DeleteCategoryByName(string name)
+        {
+            string sql = "DELETE FROM category WHERE name = @name";
+            return SqlHelper.ExecuteNonQuery(sql, new SqlParameter("@name", name));
+        }
+    }
+    
+}
---
+++ Account/Account/Model/Bill.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Account.Model
+{
+    public class Bill
+    {
+        public int BillID { get; set; }
+        public string Type { get; set; }//采用Income与Expense！！！后面逻辑如此使用。
+        public Date DateTime { get; set; }
+        public Category Category { get; set; }
+        public float Money { get; set; }
+        public int CateID { get; set; }
+        public string Remarks { get; set; }
+
+        public Bill(string type, Date dateTime, float money, int cateID, string remarks)
+        {
+            Type = type;
+            DateTime = dateTime;
+            Money = money;
+            CateID = cateID;
+            Remarks = remarks;
+        }
+
+        public Bill()
+        {
+        }
+        public string toString()
+        {
+            return "Bill{" +
+                    "BillID=" + BillID +
+                    ", Type='" + Type + '\'' +
+                    ", DateTime=" + DateTime +
+                    ", Money=" + Money +
+                    ", CateID=" + CateID +
+                    ", Remarks='" + Remarks + '\'' +
+                    '}';
+        }
+    }
+}

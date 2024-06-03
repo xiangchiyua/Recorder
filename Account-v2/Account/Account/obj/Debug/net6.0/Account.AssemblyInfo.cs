c4366be2e12ef7c24a106a931fb2e198f2bf625d
xiangchiyua <161929080+xiangchiyua@users.noreply.github.com>
2024-06-04 00:44:06 +0800

    API更新

--- Account-v2/Account/Account/obj/Debug/net6.0/Account.AssemblyInfo.cs
+++ Account-v2/Account/Account/obj/Debug/net6.0/Account.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Account")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e9d164ec9dd14dff4ce6b82c14919f4721dd5d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+759344b63fa05fdeff7a5e88dc474c55b4ed3feb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Account")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Account")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ app/Account-v2/Account/Account/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using Account.Core;
+using Account.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+using Account.Dal;
+
+namespace Account.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Bill> queryBillByOther(int billID, int cateID, string type, float money, string choice)
+        {
+            BillDal billDal = new BillDal();
+            return billDal.QueryFromBillByOther(billID,cateID,type,money,choice);
+
+        }
+        [HttpGet]
+        public ActionResult<List<Bill>> queryBillByDate(DateTime startDate,string endDate)
+        {
+
+            BillDal billDal = new BillDal();
+            return billDal.QueryFromBillByDate(startDate,endDate).ToList();
+        }
+        [HttpPost]
+        public ActionResult<long> insertBill(Bill bill)
+        {
+            BillDal billDal = new BillDal();   
+            return billDal.InseryBill(bill);
+        }
+        [HttpPost]
+        public ActionResult<long> insertCategory(Category category)
+        {
+            CategoryDal categoryDal = new CategoryDal();
+            return categoryDal.insertCategory(category);
+        }
+        [HttpPost]
+
+        [HttpDelete]
+        public ActionResult<long> deleteFromBillByID(int id)
+        {
+            BillDal billDal = new BillDal();
+            return billDal.DeleteFromBillByID(id);
+        }
+        [HttpDelete]
+        public ActionResult<long> deleteFromCategoryByName(string name)
+        {
+            CategoryDal cateDal = new CategoryDal();
+            return cateDal.DeleteCategoryByName(name);
+        }
+        
+    }
+}
